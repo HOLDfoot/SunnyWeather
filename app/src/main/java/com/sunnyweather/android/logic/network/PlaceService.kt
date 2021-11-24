d67aa1e8c3e608cd9655f6ba4ef9ca121aa00427
Zhu Yongle <iamzhulele@163.com>
2021-11-24 20:24:40 +0800

    实现搜索全球城市数据功能

--- app/src/main/java/com/sunnyweather/android/logic/network/PlaceService.kt
+++ app/src/main/java/com/sunnyweather/android/logic/network/PlaceService.kt
@@ -1,7 +1,9 @@
 package com.sunnyweather.android.logic.network
 
+import com.sunnyweather.android.SunnyWeatherApplication
 import com.sunnyweather.android.logic.model.PlaceResponse
 import retrofit2.Call
+import retrofit2.http.GET
 import retrofit2.http.Query
 
 /**
@@ -12,5 +14,6 @@ import retrofit2.http.Query
  */
 interface PlaceService {
 
+    @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
     fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
 }
