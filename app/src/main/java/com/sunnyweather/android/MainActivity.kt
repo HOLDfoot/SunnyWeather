22923341347b6e72fc414e8bff9eb2d66bd58f3d
Zhu Yongle <iamzhulele@163.com>
2021-11-30 17:21:41 +0800

    可以运行

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -1,15 +1,7 @@
 package com.sunnyweather.android
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.sunnyweather.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
